11bc6b86524896bc4363e0c79c07c210681947b7
راكان عبدالله السهلي <klr23550@gmail.com>
2026-02-10 15:11:25 +0300

    Create Flutter app with ProfilePage
    Added a Flutter app with a profile page displaying user information.

--- homework1/mina.dart
+++ homework1/mina.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp()); 
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProfilePage(), 
+    );
+  }
+}
+
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Name: Rakan Al-sahli'),
+            Text('id: 445159209'),
+          ],
+        ),
+      ),
+    );
+  }
+}
